ac02f69cf8f03c098cdcdbc10a4d1a7c574fd3b5
jatinsharma2132-launchpadapps <162543581+jatinsharma2132-launchpadapps@users.noreply.github.com>
2024-06-28 17:51:09 +0530

    file deleted

--- alpha-backend-cms-ms-v1/src/policy/policy.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PolicyController } from './policy.controller';
-import { PolicyService } from './policy.service';
-
-@Module({
-  controllers: [PolicyController],
-  providers: [PolicyService]
-})
-export class PolicyModule {}
